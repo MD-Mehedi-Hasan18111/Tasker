91547b605a6afb5595ab95d210ed34c6ae99bc41
MD Mehedi Hasan <md@bursement.com>
2024-01-14 23:18:45 +0600

    HTML Template convert to React Components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./component/Hero";
+import Navbar from "./component/Navbar";
+import TaskBoard from "./component/TaskBoard/TaskBoard";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col items-center justify-center">
+        <Hero />
+        <TaskBoard />
+      </div>
+    </>
+  );
+}
